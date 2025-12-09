1c5e8bf15701dc2267ba82ec528193016b919c8a
Nicolas Montard <nmontard@z3r5p6.42lyon.fr>
2025-12-09 13:08:56 +0100

    add swap and stack struct

---
+++ swap.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swap.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmontard <nmontard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/09 11:48:07 by nmontard          #+#    #+#             */
+/*   Updated: 2025/12/09 13:08:18 by nmontard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "stack.h"
+
+void	swap(stack *stack)
+{
+	int	temp;
+
+	if (stack->top < 1)
+		return ;
+	temp = stack->array[stack->top];
+	stack->array[stack->top] = stack->array[stack->top - 1];
+	stack->array[stack->top - 1] = temp;
+}
+
+void	ss(stack *a, stack *b)
+{
+	swap(a);
+	swap(b);
+}
