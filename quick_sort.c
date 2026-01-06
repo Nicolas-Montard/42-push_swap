b0051046978d20ada4f374a3dc118d4d60e6e70c
Nicolas Montard <nmontard@z4r9p7.42lyon.fr>
2026-01-06 16:33:41 +0100

    WIP

--- quick_sort.c
+++ quick_sort.c
@@ -6,14 +6,14 @@
 /*   By: nmontard <nmontard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 12:34:33 by nmontard          #+#    #+#             */
-/*   Updated: 2026/01/06 15:07:38 by nmontard         ###   ########.fr       */
+/*   Updated: 2026/01/06 16:28:34 by nmontard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "headers/stack.h"
 #include <stdlib.h>
 
-int	get_pivot_from_partition(t_stack *stack, int size)
+static int	get_pivot_from_partition(t_stack *stack, int size)
 {
 	t_node	*current_node;
 	int		counter;
@@ -25,66 +25,95 @@ int	get_pivot_from_partition(t_stack *stack, int size)
 		current_node = current_node->next;
 		counter++;
 	}
-	return (*current_node->value);
+	return (current_node->value);
 }
 
-// void	sort_with_partioning(t_stack *stack_a, t_stack *stack_b, int size_a,
-// 		int size_b)
-// {
-// 	int	pivot;
-// 	int	rotate_counter;
+void	sort_stack_a(t_stack *stack_a, t_stack *stack_b, int size_a)
+{
+	int	pivot;
+	int	i;
+	int	size_b;
 
-// 	// value of condition could change to use the most effective, need test
-// 	// could be interresting to change for disorder metric based
-// 	if (size_a < 6 && size_a > 0)
-// 	{
-// 		// TODO use simple algo like insertion sort
-// 		return ;
-// 	}
-// 	if (size_b < 6 && size_b > 0)
-// 	{
-// 		// either sort from the other side or push all to size_a
-// 		return ;
-// 	}
-// 	if (size_a > 6)
-// 	{
-// 		// will need to be separated as a function
-// 		while (*stack_a->head->value)
-// 		{
-// 			if (*stack_a->head->value < pivot)
-// 			{
-// 				size_a--;
-// 				size_b++;
-// 				pb(stack_b, stack_a);
-// 			}
-// 			else
-// 			{
-// 				rotate_counter++;				
-// 				ra(stack_a);
-// 			}
-// 			while (rotate_counter != 0)
-// 			{
-// 				rra(stack_a);
-// 				rotate_counter--;
-// 			}
-// 		}
-// 		sort_with_partioning(stack_a, stack_b, size_a, size_b);
-// 	}
-	
-// }
+	size_b = 0;
+	i = 0;
+	// number need to be tested to adjust them
+	if (size_a < 20)
+	{
+		// call a function to sort small array
+		return ;
+	}
+	pivot = get_pivot_from_partition(stack_a, size_a);
+	while (stack_a->head->value != pivot)
+	{
+		if (stack_a->head->value < pivot)
+		{
+			pb(stack_b, stack_a);
+			size_b++;
+			size_a--;
+		}
+		else
+			ra(stack_a);
+	}
+	pb(stack_b, stack_a);
+	while (i < size_a)
+		rra(stack_a);
+	sort_stack_a(stack_a, stack_b, size_a);
+	sort_stack_b(stack_a, stack_b, size_b);
+}
+
+sort_stack_b(t_stack *stack_a, t_stack *stack_b, int size_b)
+{
+	int	pivot;
+	int	i;
+	int	size_a;
+
+	size_a = 0;
+	i = 0;
+	pa(stack_a, stack_b);
+	// number need to be tested to adjust them
+	if (size_b < 20)
+	{
+		// call a function to sort small array and push or just push
+		return ;
+	}
+	pivot = get_pivot_from_partition(stack_b, size_b);
+	while (stack_b->head->value != pivot)
+	{
+		if (stack_b->head->value < pivot)
+		{
+			pa(stack_a, stack_b);
+			size_a++;
+			size_b--;
+		}
+		else
+			rb(stack_b);
+	}
+	pa(stack_b, stack_a);
+	while (i < size_b)
+		rrb(stack_b);
+	sort_stack_a(stack_a, stack_b, size_a);
+	sort_stack_b(stack_a, stack_b, size_b);
+}
 
 void	quick_sort(t_stack *stack_a, t_stack *stack_b)
 {
 	t_node	*start_node;
 	int		pivot;
+	int		size_b;
 
-	pivot = get_pivot_from_partition(stack_a, stack_a->size);
+	size_b = 0;
+	pivot = start_node->previous;
 	while (stack_a->head->value != pivot)
 	{
-		if (*stack_a->head->value < pivot)
+		if (stack_a->head->value < pivot)
+		{
+			size_b++;
 			pb(stack_b, stack_a);
+		}
 		else
 			ra(stack_a);
 	}
 	pb(stack_b, stack_a);
+	// call left function
+	// call right function
 }
