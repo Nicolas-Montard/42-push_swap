50f1f964fed478b66a182f6fd977b4c953fea4ac
Nicolas Montard <nmontard@z4r9p7.42lyon.fr>
2026-01-06 15:05:32 +0100

    merge resolved

--- quick_sort.c
+++ quick_sort.c
@@ -1,33 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   reverse_rotate.c                                   :+:      :+:    :+:   */
+/*   quick_sort.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nmontard <nmontard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/10 14:00:01 by aslimani          #+#    #+#             */
-/*   Updated: 2026/01/06 15:04:02 by nmontard         ###   ########.fr       */
+/*   Created: 2026/01/05 12:34:33 by nmontard          #+#    #+#             */
+/*   Updated: 2026/01/05 12:44:52 by nmontard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "headers/list_utils.h"
 #include "headers/stack.h"
-#include <stdlib.h>
 
-void    rra(t_stack *a)
+void	quick_sort(t_stack stack_a, t_stack stack_b)
 {
-    if(a->size > 1)
-        a->head = a->head->previous;
-}
-
-void    rrb(t_stack *b)
-{
-    if(b->size > 1)
-        b->head = b->head->previous;
-}
-
-void    rrr(t_stack *a, t_stack *b)
-{
-    rra(a);
-    rrb(b);
 }
