f3a5fcfe7f19d40cd989b06385229a0b9c502951
Nicolas Montard <nmontard@z4r10p2.42lyon.fr>
2026-01-14 14:29:28 +0100

    fix stack info display

--- rotate.c
+++ rotate.c
@@ -6,13 +6,13 @@
 /*   By: nmontard <nmontard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/10 13:35:03 by aslimani          #+#    #+#             */
-/*   Updated: 2026/01/14 14:16:48 by nmontard         ###   ########.fr       */
+/*   Updated: 2026/01/14 14:27:09 by nmontard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "headers/ft_printf.h"
 #include "headers/list_utils.h"
 #include "headers/stack.h"
-#include "headers/ft_printf.h"
 #include <stdlib.h>
 
 void	ra(t_stack *a)
@@ -31,7 +31,9 @@ void	rb(t_stack *b)
 
 void	rr(t_stack *a, t_stack *b)
 {
-	ra(a);
-	rb(b);
-	//ft_printf("rr\n");
+	if (a->size > 1)
+		a->head = a->head->next;
+	if (b->size > 1)
+		b->head = b->head->next;
+	ft_printf("rr\n");
 }
